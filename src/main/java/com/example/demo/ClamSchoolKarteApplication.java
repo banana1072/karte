7560160047d55ad65be8e081bd0cff7dfb2e57b2
Hayato Tachibana <h.tachibana@lulllulllull.onmicrosoft.com>
2024-06-27 23:47:39 +0900

    最初のコミット

---
+++ src/main/java/com/example/demo/ClamSchoolKarteApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClamSchoolKarteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClamSchoolKarteApplication.class, args);
+	}
+
+}
